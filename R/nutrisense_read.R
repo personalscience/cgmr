332a7aa25f959876c4c1efebae6db193442ea211
Richard Sprague <richardsprague@gmail.com>
2021-09-26 16:48:00 -0700

    Levels and Nutrisense conversion, plus tests.

--- R/nutrisense_read.R
+++ R/nutrisense_read.R
@@ -1,4 +1,4 @@
-# read Nutrisense files
+# read non-Freestyle Libre files
 
 #' @title read a Nutrisense export file and convert to the PSI CGM format
 #' @description Nutrisense export files contain timestamped information about
@@ -45,6 +45,46 @@ glucose_df_from_nutrisense <- function(filepath = system.file("extdata", package
               scan = value,
               hist = value,
               user_id = user_id)
+
+  return(glucose_raw)
+}
+
+#' @title read a Levels export file and convert to the PSI CGM format
+#' @description Levels export files contain timestamped information about
+#' glucose values, meals, exercise, sleep, and more.
+#' Important: This function expects an extra line at the top of the Nutrisense raw file.
+#' The first line must say "Levels" and include the full name of the person.
+#' @param filepath path to a valid Levels CSV data file
+#' @param user_id new user ID to be appended to the dataframe
+#' @param tz time zone
+#' @return a canonical glucose value dataframe
+#' @export
+glucose_df_from_levels <- function(filepath = system.file("extdata", package = "cgmr", "Firstname4Lastname4_levels.csv"),
+                                       user_id = 3000, # placeholder
+                                       tz = "UTC") {
+
+
+  firstline <- readLines(con = filepath, 1) %>%
+    str_split(pattern = ",", simplify = TRUE)
+
+  if(firstline[1] != "Levels") return(NULL)  # Not a Levels file
+
+  skip_lines <- 1
+
+  glucose_raw <-
+    readr::read_csv(filepath, skip=skip_lines, show_col_types = FALSE) %>%
+    transmute(time = `Time (UTC)`,
+              value = ifelse(`Type of Reading` == "timeseries",
+                             `Glucose Reading (mg/dL)`,
+                             `Glucose Reading (mg/dL)`),
+              scan = ifelse(`Type of Reading` == "directread",
+                            `Glucose Reading (mg/dL)`,
+                            NA),
+              hist = ifelse(`Type of Reading` == "timeseries",
+                            `Glucose Reading (mg/dL)`,
+                            `Glucose Reading (mg/dL)`),
+              user_id = user_id)
+
 
   return(glucose_raw)
 }
---
+++ tests/testthat/test-nutrisense_read.R
@@ -0,0 +1,15 @@
+
+sample_levels_path <- system.file("extdata", package = "cgmr", "Firstname4Lastname4_levels.csv")
+
+sample_nutrisense_path <- system.file("extdata", package = "cgmr", "Firstname3Lastname3_nutrisense.csv")
+
+levels_results <- glucose_df_from_levels(sample_levels_path)
+nutrisense_results <- glucose_df_from_nutrisense(sample_nutrisense_path)
+
+test_that("Levels conversion seems reasonable",{
+  expect_equal(levels_results %>% head() %>% pull(value), c(106,106,102,102,100,106))
+})
+
+test_that("Nutrisense conversion seems reasonable",{
+  expect_equal(nrow(nutrisense_results),16052)
+})
